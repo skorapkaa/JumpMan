8cb9501230b55eb122019f1a11ed67179b7431c4
skorapkaa <dejv.chvatal@seznam.cz>
2025-05-14 18:33:30 +0200

    pokus o kolizi marne :D postava se teleportuje

--- src/character.py
+++ src/character.py
@@ -12,10 +12,10 @@ class Character:
         self.on_ground = False
         self.facing_direction = "right"
 
-        self.character_stand_right = pygame.image.load("../assets/sprites/character_sprites/stand_sprite_right.png")
+        self.character_stand_right = pygame.image.load("assets/sprites/character_sprites/stand_sprite_right.png")
         self.character_stand_right = pygame.transform.scale(self.character_stand_right, (40, 60))
 
-        self.character_stand_left = pygame.image.load("../assets/sprites/character_sprites/stand_sprite_left.png")
+        self.character_stand_left = pygame.image.load("assets/sprites/character_sprites/stand_sprite_left.png")
         self.character_stand_left = pygame.transform.scale(self.character_stand_left, (40, 60))
 
     def draw(self, screen):
@@ -27,18 +27,45 @@ class Character:
     def get_rect(self):
         return pygame.Rect(self.x, self.y, 40, 60)
 
-    def update(self):
+    def update(self, terrain):
+        self.on_ground = False
+        self.x += self.velocity_x
+        player_rect = self.get_rect()
+        
+        for rect in terrain:
+            if player_rect.colliderect(rect):
+                if self.velocity_x > 0:
+                    self.x = rect.left - player_rect.width
+                elif self.velocity_x < 0:
+                    self.x = rect.right
+                self.velocity_x = 0
+                player_rect = self.get_rect()
+
         self.velocity_y += self.gravity
         self.y += self.velocity_y
+        player_rect = self.get_rect()
 
-        # postava neopustí obrazovku
-        self.x = max(0, min(self.x, 800 - 40))
+        for rect in terrain:
+            if player_rect.colliderect(rect):
+                if self.velocity_y > 0:
+                    self.y = rect.top - player_rect.height
+                    self.velocity_y = 0
+                    self.on_ground = True
+                elif self.velocity_y < 0:
+                    self.y = rect.bottom
+                    self.velocity_y = 0
+                    
+        if self.y > 700:
+            self.alive = False
+        
+              
+
 
     def move(self, direction):
-        if direction == "left":
+        if direction == "left" and self.x > 0:
             self.x -= self.speed
             self.facing_direction = "left"
-        elif direction == "right":
+        elif direction == "right" and self.x < 760:
             self.x += self.speed
             self.facing_direction = "right"
 
@@ -47,10 +74,4 @@ class Character:
             self.velocity_y = -10
             self.on_ground = False
 
-    def check_collision(self, terrain):
-        self.on_ground = False
-        for rect in terrain:
-            if pygame.Rect(self.x, self.y + 60, 40, 1).colliderect(rect):
-                self.on_ground = True
-                self.velocity_y = 0
-                self.y = rect.top - 60
+
