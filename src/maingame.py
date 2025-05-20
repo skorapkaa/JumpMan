a9e41ea9f63658afc8b0a26898cafe1ae43d0bd4
skorapkaa <dejv.chvatal@seznam.cz>
2025-05-20 16:41:22 +0200

    menu veci

--- src/maingame.py
+++ src/maingame.py
@@ -4,11 +4,13 @@ import pygame
 from button import Button
 from player import Player
 from terrain import Terrain
+import os
+os.environ['SDL_VIDEO_CENTERED'] = '1'      #toto nam umozni menit rozliseni bez pohybu okna
 
 pygame.init()
 
 def get_font(size):
-    return pygame.font.Font("assets/fonts/chrustyrock.ttf", size)
+    return pygame.font.Font("assets/fonts/PlayfulTime.ttf", size)
 
 class MainGame:
     def __init__(self):
@@ -18,14 +20,33 @@ class MainGame:
         pygame.display.set_caption("Jumper")
         self.clock = pygame.time.Clock()
 
+        self.coin_grab = pygame.mixer.Sound("assets/sounds/coin.wav")
+        self.coin_grab.set_volume(0.2)
+
         self.menu_birds_sfx = pygame.mixer.Sound("assets/sounds/birds.wav")
         self.menu_pop_btn_sfx = pygame.mixer.Sound("assets/sounds/pop.wav")
         self.menu_pop_btn_sfx.set_volume(0.2)
 
-        self.menu_background = pygame.transform.scale(
-            pygame.image.load("assets/sprites/menu_sprites/background_menu.png").convert(),
-            (800, 600)
-        )
+        self.menu_song = pygame.mixer.Sound("assets/music/Oceania.mp3")
+        self.menu_song.set_volume(0.05)
+
+        self.power_up = pygame.mixer.Sound("assets/sounds/power_up.wav")
+        self.power_up.set_volume(0.2)
+
+        self.menu_backgrounds = {
+            (800, 600): pygame.transform.scale(
+                pygame.image.load("assets/sprites/menu_sprites/background_menu_800_600.png").convert(), (800, 600)
+            ),
+            (1024, 768): pygame.transform.scale(
+                pygame.image.load("assets/sprites/menu_sprites/background_menu_1024_768.png").convert(), (1024, 768)
+            ),
+            (1280, 720): pygame.transform.scale(
+                pygame.image.load("assets/sprites/menu_sprites/background_menu_1280_720.png").convert(), (1280, 720)
+            ),
+        }
+
+        self.scaled_background = self.menu_backgrounds[(self.WIDTH, self.HEIGHT)]
+
 
     def play(self, difficulty="medium"):
         screen_run = True
@@ -59,7 +80,7 @@ class MainGame:
 
         RETURN_BUTTON = Button(
             image=pygame.image.load("assets/sprites/menu_sprites/Options Rect.png"),
-            pos=(400, 300),
+            pos=(self.WIDTH//2, 300),
             text_input="RETURN TO MENU",
             font=get_font(40),
             base_color="#1C86E5",
@@ -78,6 +99,7 @@ class MainGame:
                 if paused and event.type == pygame.MOUSEBUTTONDOWN:
                     if RETURN_BUTTON.checkForInput(pygame.mouse.get_pos()):
                         self.menu_pop_btn_sfx.play()
+                        self.menu_birds_sfx.stop()
                         return  # návrat do menu
 
             if not paused:
@@ -85,7 +107,6 @@ class MainGame:
                 player.update(ground_rects + platform_rects)
 
             if not player.alive:
-                print("propast")
                 pygame.time.wait(500)
                 player = Player()
                 terrain = Terrain()
@@ -117,6 +138,7 @@ class MainGame:
             # Vykreslení mincí a podminka pro jejich sebrani
             for coin in item_rects[:]:
                 if player.get_rect().colliderect(coin):
+                    self.coin_grab.play()
                     item_rects.remove(coin)
                     score += 1
                 else:
@@ -126,6 +148,7 @@ class MainGame:
             for boost in boost_rects:
                 if player.get_rect().colliderect(boost):
                     boost_rects.remove(boost)    
+                    self.power_up.play()
                     player.jump_boost_end_time = pygame.time.get_ticks() + 5000  # boost na 5 sekund
                 else:
                     self.SCREEN.blit(boost_texture, (boost.x - scroll_x, boost.y))    
@@ -160,12 +183,40 @@ class MainGame:
 
     def options(self):
         screen_run = True
+
         BACK_BUTTON = Button(
             image=pygame.image.load("assets/sprites/menu_sprites/Play Rect.png"),
-            pos=(400, 250),
+            pos=(self.WIDTH//2, 500),
             text_input="BACK",
             font=get_font(60),
             base_color="#1C86E5",
+            hovering_color="White"
+        )
+
+        RES_800x600 = Button(
+            image=pygame.image.load("assets/sprites/menu_sprites/Options Rect.png"),
+            pos=(self.WIDTH//2, 150),
+            text_input="800x600",
+            font=get_font(40),
+            base_color="#FFD700",
+            hovering_color="White"
+        )
+
+        RES_1024x768 = Button(
+            image=pygame.image.load("assets/sprites/menu_sprites/Options Rect.png"),
+            pos=(self.WIDTH//2, 250),
+            text_input="1024x768",
+            font=get_font(40),
+            base_color="#FFD700",
+            hovering_color="White"
+        )
+
+        RES_1280x720 = Button(
+            image=pygame.image.load("assets/sprites/menu_sprites/Options Rect.png"),
+            pos=(self.WIDTH//2, 350),
+            text_input="1280x720",
+            font=get_font(40),
+            base_color="#FFD700",
             hovering_color="White"
         )
 
@@ -176,33 +227,50 @@ class MainGame:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if BACK_BUTTON.checkForInput(MOUSE_POS):
                         self.menu_pop_btn_sfx.play()
                         return
 
-            self.SCREEN.fill((0, 0, 255))
-            BACK_BUTTON.changeColor(MOUSE_POS)
-            BACK_BUTTON.update(self.SCREEN)
+                    if RES_800x600.checkForInput(MOUSE_POS):
+                        self.change_resolution(800, 600)
+                        return
+
+
+                    if RES_1024x768.checkForInput(MOUSE_POS):
+                        self.change_resolution(1024, 768)
+                        return
+
+                    if RES_1280x720.checkForInput(MOUSE_POS):
+                        self.change_resolution(1280, 720)
+                        return
+
+            self.SCREEN.fill((255, 255, 255))
+            for button in [RES_800x600, RES_1024x768, RES_1280x720, BACK_BUTTON]:
+                button.changeColor(MOUSE_POS)
+                button.update(self.SCREEN)
 
             pygame.display.update()
             self.clock.tick(60)
 
     def menu(self):
+
         if not pygame.mixer.get_busy():
-            self.menu_birds_sfx.play(loops=-1)
+            self.menu_song.play(loops=-1)
 
         while True:
-            self.SCREEN.blit(self.menu_background, (0, 0))
+
+            self.SCREEN.blit(self.scaled_background, (0, 0))
             MENU_MOUSE_POS = pygame.mouse.get_pos()
 
             MENU_TEXT = get_font(100).render("MAIN MENU", True, "#04049B")
-            MENU_RECT = MENU_TEXT.get_rect(center=(400, 100))
+            MENU_RECT = MENU_TEXT.get_rect(center=(self.WIDTH // 2, 100))
             self.SCREEN.blit(MENU_TEXT, MENU_RECT)
 
             PLAY_BUTTON = Button(
                 image=pygame.image.load("assets/sprites/menu_sprites/Play Rect.png"),
-                pos=(400, 250),
+                pos=(self.WIDTH//2, 250),
                 text_input="PLAY",
                 font=get_font(60),
                 base_color="#1C86E5",
@@ -210,7 +278,7 @@ class MainGame:
             )
             OPTIONS_BUTTON = Button(
                 image=pygame.image.load("assets/sprites/menu_sprites/Options Rect.png"),
-                pos=(400, 400),
+                pos=(self.WIDTH//2, 400),
                 text_input="OPTIONS",
                 font=get_font(50),
                 base_color="#1C86E5",
@@ -218,7 +286,7 @@ class MainGame:
             )
             QUIT_BUTTON = Button(
                 image=pygame.image.load("assets/sprites/menu_sprites/Quit Rect.png"),
-                pos=(400, 550),
+                pos=(self.WIDTH//2, 550),
                 text_input="QUIT",
                 font=get_font(50),
                 base_color="#1C86E5",
@@ -236,9 +304,9 @@ class MainGame:
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
                         self.menu_pop_btn_sfx.play()
-                        self.menu_birds_sfx.stop()
+                        self.menu_song.stop()
                         self.play()
-                        self.menu_birds_sfx.play(loops=-1)
+                        self.menu_song.play(loops=-1)
                     if OPTIONS_BUTTON.checkForInput(MENU_MOUSE_POS):
                         self.menu_pop_btn_sfx.play()
                         self.options()
@@ -248,3 +316,16 @@ class MainGame:
 
             pygame.display.update()
             self.clock.tick(60)
+
+    def change_resolution(self, width, height):
+        self.WIDTH = width
+        self.HEIGHT = height
+        os.environ['SDL_VIDEO_CENTERED'] = '1'
+        self.SCREEN = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
+
+        if (width, height) in self.menu_backgrounds:
+            self.scaled_background = self.menu_backgrounds[(width, height)]
+        else:
+            self.scaled_background = pygame.transform.smoothscale(
+                list(self.menu_backgrounds.values())[0], (width, height)
+            )
