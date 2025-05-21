88e74dc3a116672eb7d3cc0d2b7a5d06738db4e3
skorapkaa <dejv.chvatal@seznam.cz>
2025-05-21 20:08:53 +0200

    upraveno

--- src/player.py
+++ src/player.py
@@ -1,26 +1,20 @@
-import pygame
-
 from character import Character
-
+import pygame
 
 class Player(Character):
     def __init__(self):
         super().__init__()
-        self.jump_boost_end_time = 0  # čas na boost
-
-    def check_collisions(self, items, terrain):
-        self.check_collision(terrain)
-        for item in items:
-            if self.get_rect().colliderect(item.itemBounds):
-                item.collectItem()
-
+        self.jump_boost_end_time = 0
 
     def handle_input(self, offset_x=0, screen_width=800):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
             self.move("left", screen_width, offset_x)
-        if keys[pygame.K_RIGHT]:
+        elif keys[pygame.K_RIGHT]:
             self.move("right", screen_width, offset_x)
+        else:
+            self.velocity_x = 0
+
         if keys[pygame.K_SPACE] or keys[pygame.K_UP]:
             self.jump()
 
@@ -28,7 +22,13 @@ class Player(Character):
         current_time = pygame.time.get_ticks()
         if self.on_ground:
             if current_time < self.jump_boost_end_time:
-                self.velocity_y = -18  # Boost skok
+                self.velocity_y = -18
             else:
-                self.velocity_y = -10  # normální skok
+                self.velocity_y = -10
             self.on_ground = False
+
+    def check_collisions(self, items, terrain):
+        self.check_collision(terrain)
+        for item in items:
+            if self.get_rect().colliderect(item.itemBounds):
+                item.collectItem()
